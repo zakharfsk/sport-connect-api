c308862a019583638ebbd3b1ad3ae53fac1349a2
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-01-27 20:36:50 +0200

    Adjust template directory in settings.py
    Changed the template directory setting in the sport_connect_api/settings.py file from 'templates' to 'BASE_DIR / templates'. This ensures that templates are loaded from the correct directory relative to the base directory of the project.

--- sport_connect_api/settings.py
+++ sport_connect_api/settings.py
@@ -99,7 +99,7 @@ ROOT_URLCONF = 'sport_connect_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
