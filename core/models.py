9a01e8abb228c612d07a55e20c50ddb23305c908
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-01-27 16:42:24 +0200

    Update verbose names for models and fields
    The verbose names for various models and fields including 'Sport', 'SportSchool', 'AverageValuesStandards', and 'WeightingFactors' in core/migrations/0001_initial.py and core/models.py have been updated for better identification and readability. Additionally, 'name_standard' field in the 'AverageValuesStandards' model has choices populated from the new 'LIST_STANDARDS' in settings.

--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 from sport_connect_api.models import BaseModel
 
 
 class Sport(BaseModel):
-    name = models.CharField(max_length=225)
+    name = models.CharField(max_length=225, verbose_name="Вид спорту")
 
     def __str__(self):
         return self.name
@@ -16,12 +17,12 @@ class Sport(BaseModel):
 
 
 class SportSchool(BaseModel):
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name="sport_schools")
-    school_name = models.CharField(max_length=350, null=False, blank=False)
-    school_site_url = models.URLField(null=False, blank=False)
-    school_sport_address = models.CharField(max_length=350, null=False, blank=False)
-    school_sport_email = models.EmailField(null=True, blank=True)
-    school_sport_phone_numbers = models.CharField(max_length=500, null=True, blank=True)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name="sport_schools", verbose_name="Вид спорту")
+    school_name = models.CharField(max_length=350, null=False, blank=False, verbose_name="Назва ДЮСШ")
+    school_site_url = models.URLField(null=False, blank=False, verbose_name="Сайт ДЮСШ")
+    school_sport_address = models.CharField(max_length=350, null=False, blank=False, verbose_name="Адреса ДЮСШ")
+    school_sport_email = models.EmailField(null=True, blank=True, verbose_name="Електронна адреса ДЮСШ")
+    school_sport_phone_numbers = models.CharField(max_length=500, null=True, blank=True, verbose_name="Телефони ДЮСШ")
 
     def __str__(self):
         return self.school_name
@@ -33,7 +34,7 @@ class SportSchool(BaseModel):
 
 
 class AverageValuesStandards(BaseModel):
-    name_standard = models.CharField(max_length=225, verbose_name="Показник")
+    name_standard = models.CharField(max_length=225, choices=((standard, standard) for standard in settings.LIST_STANDARDS), verbose_name="Показник")
     children_age = models.IntegerField(choices=[(i, i) for i in range(10, 12)], verbose_name="Вік дитини", null=True)
     children_gender = models.CharField(max_length=225, choices=[("Юнак", "Юнак"), ("Дівчина", "Дівчина")],
                                        verbose_name="Стать дитини", null=True)
@@ -50,9 +51,10 @@ class AverageValuesStandards(BaseModel):
 
 
 class WeightingFactors(BaseModel):
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name="weighting_factors")
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name="weighting_factors",
+                              verbose_name="Вид спорту")
     average_value_standard = models.ForeignKey(AverageValuesStandards, on_delete=models.CASCADE,
-                                               related_name="weighting_factors")
+                                               related_name="weighting_factors", verbose_name="Норматив")
     weighting_factor = models.FloatField(verbose_name="Ваговий коефіцієнт")
 
     class Meta:
