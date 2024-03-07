59327b885ef9902532f83dfb1c10b6792ceb63a4
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-03-07 23:42:28 +0200

    Fix typo in python manage.py path in django-entrypoint.sh
    Corrected a typo in the file path to the python manage.py in the django-entrypoint.sh file. Now, it points to the correct location at 'sport_connect_api/manage.py', which should resolve any issues concerning access to the manage.py file during execution.

--- entrypoints/prod/django-entrypoint.sh
+++ entrypoints/prod/django-entrypoint.sh
@@ -4,16 +4,16 @@ set -o errexit
 set -o nounset
 
 echo "Migrate collect static..."
-python ./sport-connect-api/manage.py collectstatic --noinput &&
+python ./sport_connect_api/manage.py collectstatic --noinput &&
 
 echo "Migrate..."
-python ./sport-connect-api/manage.py migrate --noinput &&
+python ./sport_connect_api/manage.py migrate --noinput &&
 
 echo "Create superuser..."
-python ./sport-connect-api/manage.py createsuperuser --noinput &&
+python ./sport_connect_api/manage.py createsuperuser --noinput &&
 
 echo "Load fixtures..."
-python -Xutf8 ./sport-connect-api/manage.py loaddata ./fixtures/core.json &&
+python -Xutf8 ./sport_connect_api/manage.py loaddata ./fixtures/core.json &&
 
 echo "Starting server..."
 gunicorn 'sport_connect_api.wsgi' --reload --bind=0.0.0.0:8000
