0b5a50e42de82260bef1fe179b44b8ae3a593cc9
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-03-07 17:03:59 +0200

    Update relative paths in django-entrypoint.sh
    Python command paths in the django-entrypoint.sh script have been updated to be relative instead of absolute. This change ensures that the commands are executed correctly regardless of the directory structure and setup of the runtime environment.

--- entrypoints/prod/django-entrypoint.sh
+++ entrypoints/prod/django-entrypoint.sh
@@ -4,16 +4,16 @@ set -o errexit
 set -o nounset
 
 echo "Migrate collect static..."
-python /sport-connect-api/manage.py collectstatic --noinput &&
+python ./sport-connect-api/manage.py collectstatic --noinput &&
 
 echo "Migrate..."
-python /sport-connect-api/manage.py migrate --noinput &&
+python ./sport-connect-api/manage.py migrate --noinput &&
 
 echo "Create superuser..."
-python /sport-connect-api/manage.py createsuperuser --noinput &&
+python ./sport-connect-api/manage.py createsuperuser --noinput &&
 
 echo "Load fixtures..."
-python -Xutf8 /sport-connect-api/manage.py loaddata ./fixtures/core.json &&
+python -Xutf8 ./sport-connect-api/manage.py loaddata ./fixtures/core.json &&
 
 echo "Starting server..."
-gunicorn 'sport-connect-api.sport_connect_api.wsgi' --reload --bind=0.0.0.0:8000
+gunicorn 'sport_connect_api.wsgi' --reload --bind=0.0.0.0:8000
