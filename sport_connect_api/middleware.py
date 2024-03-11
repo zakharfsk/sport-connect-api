b7abdbcdf05077a61fa9bd46ffc8830dad91d081
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-03-11 15:59:13 +0200

    Remove TokenMiddleware from middleware.py
    The TokenMiddleware class in the 'sport_connect_api/middleware.py' file has been deleted due to redundancy. The functionality was found to be unnecessary, resulting in its removal to optimize code and enhance application performance.

--- sport_connect_api/middleware.py
+++ sport_connect_api/middleware.py
@@ -1,20 +1,6 @@
 from django.core.handlers.wsgi import WSGIRequest
 
 
-class TokenMiddleware:
-    async def __call__(self, scope, receive, send):
-        query_string = scope.get("query_string", b"").decode("utf-8")
-        query_parameters = {key: value for key, value in (
-            param.split("=") for param in query_string.split("&"))}
-        token = query_parameters.get("token", None)
-        scope["token"] = token
-        # Проводим аутентификацию пользователя на основе токена
-
-        # Добавляем пользователя в scope для дальнейшего использования в consumers
-
-        return await super().__call__(scope, receive, send)
-
-
 class JWTAuthMiddleware:
     def __init__(self, get_response):
         self._get_response = get_response
