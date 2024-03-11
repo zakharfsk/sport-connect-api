956247c9d79d6e3aa2c1174dab2549b7e2fbf3cb
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-03-11 17:11:31 +0200

    Add 'results' path and namespace to urls.py
    A 'results' path and namespace 'core_api' have been added to the 'urls.py' configuration. The modification also includes cleaning out the unnecessary comments and hence streamlining the file. The addition of the new URL pattern will help to better organize and manage the application's URLs.

--- sport_connect_api/urls.py
+++ sport_connect_api/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for sport_connect_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,7 +11,8 @@ urlpatterns = [
     path('', lambda request: redirect('schema-swagger-ui', permanent=True)),
     path('api/v1/', include([
         path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        path('', include('users.api.urls', namespace='authorization'))
+        path('', include('users.api.urls', namespace='authorization')),
+        path('results/', include('core.api.urls', namespace='core_api'))
     ])),
     path('calculations/', include('core.urls', namespace='core')),
 ]
