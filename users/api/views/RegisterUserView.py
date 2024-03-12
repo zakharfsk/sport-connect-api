3211aa31e70fa227b0e2ad4944fd4bc3d133571b
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-03-12 15:00:13 +0200

    Implement user and school-related views
    Created new views for user registration, login and profile display, in `users/api/views` directory. Similarly, implemented a view to list all available schools. These views use the previously defined serializers to handle request data and offer responses in a structured form.

---
+++ users/api/views/RegisterUserView.py
@@ -0,0 +1,25 @@
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.api.serializers import RegisterUserSerializer
+from users.models import User
+
+__all__ = ('RegisterUserView',)
+
+
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = RegisterUserSerializer
+    queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        user = get_object_or_404(User, email=request.data['email'])
+
+        tokens = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh': str(tokens),
+            'access': str(tokens.access_token),
+        })
---
+++ users/api/views/UserModelViewSet.py
@@ -0,0 +1,26 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from users.api.serializers import ShowUserSerializer
+
+
+__all__ = ('UserModelViewSet',)
+
+class UserModelViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    @swagger_auto_schema(responses={200: openapi.Response('User info', ShowUserSerializer)})
+    def me(self, request):
+        serializer = ShowUserSerializer(request.user)
+        return Response(serializer.data)
+
+    @me.mapping.delete
+    def delete_me(self, request):
+        user = request.user
+        user.delete()
+        return Response(status=204)
