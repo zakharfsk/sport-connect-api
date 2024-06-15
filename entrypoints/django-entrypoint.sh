a0c07f4e6b71b16a9aa83c8141fabf634c69783a
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-06-15 15:33:49 +0300

    update

--- entrypoints/django-entrypoint.sh
+++ entrypoints/django-entrypoint.sh
@@ -6,14 +6,8 @@ set -o nounset
 echo "Collect static..."
 python manage.py collectstatic --noinput &&
 
-#echo "Migrate..."
-#python manage.py migrate --noinput &&
-#
-#echo "Create superuser..."
-#python manage.py createsuperuser --noinput &&
-
-#echo "Load fixtures..."
-#python -Xutf8 manage.py loaddata ./fixtures/core.json &&
+echo "Migrate..."
+python manage.py migrate --noinput &&
 
 echo "Starting server..."
 gunicorn 'sport_connect_api.wsgi' --reload --bind=0.0.0.0:8000
