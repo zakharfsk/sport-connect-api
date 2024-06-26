02efb5d731cc8efaaa02965303195c6fe552f8f8
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-06-26 20:31:28 +0300

    update

--- core/models/WeightingFactors.py
+++ core/models/WeightingFactors.py
@@ -8,12 +8,12 @@ __all__ = ('WeightingFactors',)
 class WeightingFactors(BaseModel):
     sport = models.ForeignKey('core.Sport', on_delete=models.CASCADE, related_name="weighting_factors",
                               verbose_name="Вид спорту")
-    average_value_standard = models.ForeignKey('core.SportStandard', on_delete=models.CASCADE,
+    sport_standard = models.ForeignKey('core.SportStandard', on_delete=models.CASCADE,
                                                related_name="weighting_factors", verbose_name="Норматив")
     weighting_factor = models.FloatField(verbose_name="Ваговий коефіцієнт")
 
     def __str__(self):
-        return f"{self.sport} - {self.average_value_standard.standard.name} - {self.weighting_factor}"
+        return f"{self.sport} - {self.sport_standard.standard} - {self.weighting_factor}"
 
     class Meta:
         db_table = "weighting_factors"
