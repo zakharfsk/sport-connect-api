3f38a36e3abaae18a71a4b0065105c987fb371c7
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-09-11 21:38:10 +0300

    Reorder fixture loading after migration
    Move the fixture loading step to occur after database migrations. This ensures that the database schema is fully prepared before loading fixture data, preventing potential issues with data integrity.

--- entrypoints/django-entrypoint.sh
+++ entrypoints/django-entrypoint.sh
@@ -6,14 +6,14 @@ set -o nounset
 echo "Collect static..."
 python manage.py collectstatic --noinput &&
 
-echo "Load fixtures..."
-python manage.py loaddata fixtures/sport.json fixtures/sport_school.json fixtures/sport_standards.json fixtures/avarage_values_standards.json fixtures/weightingfactors.json fixtures/users_and_school.json
-
 echo "Make migrations..."
 python manage.py makemigrations &&
 
 echo "Migrate..."
 python manage.py migrate --noinput &&
+
+echo "Load fixtures..."
+python manage.py loaddata fixtures/sport.json fixtures/sport_school.json fixtures/sport_standards.json fixtures/avarage_values_standards.json fixtures/weightingfactors.json fixtures/users_and_school.json
 
 echo "Starting server..."
 gunicorn 'sport_connect_api.wsgi' --reload --bind=0.0.0.0:8000
